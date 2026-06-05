d9b878043dd391e94f1b20b781694679721593bc
Ahmedsawan42 <ahmed42lux@gmail.com>
2026-06-05 19:21:10 +0000

    project19

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    people = ['Farrah', 'Fred', 'Felicia']
+    fruits = ['apples', 'bananas', 'oranges', 'peaches']
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+
+    # Create bottom array to stack the bars
+    bottom = np.zeros(3)
+
+    # Plot each fruit as a stacked bar
+    for i in range(len(fruits)):
+        plt.bar(people, fruit[i], width=0.5, bottom=bottom,
+                color=colors[i], label=fruits[i])
+        bottom += fruit[i]
+
+    plt.ylabel('Quantity of Fruit')
+    plt.yticks(np.arange(0, 81, 10))
+    plt.ylim(0, 80)
+    plt.title('Number of Fruit per Person')
+    plt.legend()
+    plt.show()
---
+++ math/plotting/README.md
@@ -0,0 +1,15 @@
+# Plotting
+
+0. Line Graph: Source code to plot y as a line graph.
+
+1. Scatter: Source code to plot x ↦ y as a scatter plot.
+
+2. Change of scale: Source code to plot x ↦ y as a line graph.
+
+3. Two is better than one: Source code to plot x ↦ y1 and x ↦ y2 as line graph
+
+4. Frequency: Source code to plot a histogram of student scores for a project.
+
+5. All in One: Source code to plot all 5 previous graphs in one figure.
+
+6. Stacking Bars: Source code to plot a stacked bar graph.
