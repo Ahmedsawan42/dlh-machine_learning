ecfa2fdaf7f5218a303d113f4ecfcba01c353fd5
Ahmedsawan42 <ahmed42lux@gmail.com>
2026-08-08 21:47:01 +0000

    project29_tsk1_mod1

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -18,28 +18,25 @@ class Neuron:
             raise ValueError("nx must be positive")
 
         # Initialize private weights with random normal distribution (2D array)
-        self._W = np.random.randn(1, nx)
+        self.__W = np.random.randn(1, nx)
 
         # Initialize private bias to 0
-        self._b = 0
+        self.__b = 0
 
         # Initialize private activated output to 0
-        self._A = 0
+        self.__A = 0
 
-    # Getter for W
     @property
     def W(self):
-        """Get the weights vector for the neuron."""
-        return self._W
+        """Get the weights vector of the neuron."""
+        return self.__W
 
-    # Getter for b
     @property
     def b(self):
-        """Get the bias for the neuron."""
-        return self._b
+        """Get the bias of the neuron."""
+        return self.__b
 
-    # Getter for A
     @property
     def A(self):
         """Get the activated output of the neuron (prediction)."""
-        return self._A
+        return self.__A
